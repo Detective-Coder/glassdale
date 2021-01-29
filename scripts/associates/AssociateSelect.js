277c65b0d4e7d7a443a487a80168c2b5ae8efdf3
Detective-Coder <aaronfrankenfield@gmail.com>
2021-01-29 10:49:14 -0500

    styled the associates pop up

--- scripts/associates/AssociateSelect.js
+++ scripts/associates/AssociateSelect.js
@@ -41,49 +41,14 @@ eventHub.addEventListener("click", (eventObject) => {
     console.log(eventObject.target.id.split("-")[1])
     if (eventObject.target.id.includes("associates-")) {
       let singleCriminal = criminal.find(x => x.id == eventObject.target.id.split("-")[1])
-      console.log(singleCriminal.known_associates);
-      console.log(singleCriminal.known_associates[0].name)
-      console.log(singleCriminal.known_associates[0].alibi)
+     
       let associateName = singleCriminal.known_associates[0].name
       let associateAlibi = singleCriminal.known_associates[0].alibi
-      console.log(associateName)
-      console.log(associateAlibi)
-      associatesBox.innerHTML = `Name: ${associateName}<br> Alibi: ${associateAlibi}`;
-      // alert(JSON.stringify(singleCriminal.known_associates));
+     
+      associatesBox.innerHTML = `<div class="name-container">Name: ${associateName}</div> <div class="alibi-container">Alibi: ${associateAlibi}</div>`;
     }
-    
   })
-
- 
-  // function Associates(associatesObject){
-  //   return `
-  //   <section class="associates-card">
-  //     <div class="associates-card-text">
-  //       <p>Name: ${singleCriminal.known_associates[0].name}</p>
-  //       <p>Alibi: ${singleCriminal.known_associates[0].alibi}</p>
-  //     </div>
-  //   </section>`
-  // }
-  
-  
-    
-  
-  
-  
-  // if the change happens in the dropdown with the id of crimeSelect(the crime filter dropdown)
-  if(eventObject.target.class === "associate"){
-      
-    
-    // for(let i = 0; i < knownAssociatesForCriminal.length; i++) {
-
-    // }
-  }
-  
 })
 
-// When the user clicks the button, you must iterate the array of known_associates for that criminal - CHECK
-// then display the following information. You can display it in a dialog box, as a sidebar, at the top of the screen, or wherever you like.
 
-// The name of the known associate
-// The alibi that the known associate is providing for the criminal to try to prove the criminals' innocence.
 
